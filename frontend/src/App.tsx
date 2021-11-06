c0c1dabf6b93cd354570522feb4a95f28317a1b9
clefersilva <clefersilva@gmail.com>
2021-11-06 20:13:12 -0200

    Basic Static Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import 'assets/css/styles.css';
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div>
+    <>
+    <NavBar />
+    <div className="container">
       <h1 className="text-primary">Olá mundo!</h1>
+
+      <DataTable
     </div>
+    <Footer />
+    </>
   );
 }
 
